a767679b0e80e3068ee8b51c70035137e37cbb05
Christian Anese <christian.anese@gmail.com>
2021-10-15 08:27:29 +0200

    Remove old index page

--- data/home.js
+++ data/home.js
@@ -204,4 +204,107 @@ export const it = {
   },
 };
 
-export default { de, it };
+export const en = {
+  hero: {
+    imageAlt: "Foto des Hofes",
+  },
+  intro: {
+    title: "Unser Neuanfang",
+    description:
+      "Im Jahr 2019 haben wir unseren Hof Ortus fertiggestellt und uns so einen langersehnten Traum verwirklicht. Das Haus wurde als KlimaHaus Nature errichtet, ein Haus, das die moderne Architektur mit der schlichten Erhabenheit der Natur verbindet. Wichtig waren uns dabei die Energieeffizienz, die Nachhaltigkeit und Natürlichkeit der Baumaterialien, um ein angenehmes und gesundes Raumklima zu schaffen - Voraussetzung für das Wohlbefinden und die Gesundheit seiner Bewohner.",
+    button: "Mehr dazu",
+  },
+  slider: {
+    title: "Unser Neuanfang",
+    text: "Auf unserem Hof leben mehrere Generationen. Wir sind der Überzeugung, dass diese Symbiose zwischen Jung und Alt, zwischen Vergangenheit und Zukunft die Gegenwart bereichert und möchten das Gefühl eines Mehrgenerationenurlaubes auch unseren Gästen mit unseren 2 Ferienwohnungen ermöglichen, welche den unterschiedlichen Bedürfnissen ihrer Bewohner gerecht werden.",
+  },
+  events: {
+    trautmannsdorf: {
+      title: "Trautmannsdorf",
+      excerpt: "Die weltbekannten Gärten von Schloss Trautmannsdorf",
+      text: "Begeistert dich die botanische Vielfalt Südtirols? Dann bist du hier genau richtig denn 80 Gartenlandschaften aus aller Welt mit zahlreichen einzigartigen Erlebnisstationen warten auf dich. Die Gärten zählen zu den schönsten Grünanlagen weltweit.",
+      hero: {
+        path: "/slider/tinified/trauti.jpg",
+        width: 1072,
+        height: 712,
+        imageAlt: "Foto des Schloss Trautmannsdorf",
+      },
+    },
+    nals: {
+      title: "Nals und Umgebung",
+      excerpt: "Die weltbekannten Gärten von Schloss Trautmannsdorf",
+      text: "Nals ist für seine lange Geschichte und seine Siedlungsspuren aus vorromanischer Zeit bekannt. Im hohen Mittelalter blühte das Dorf aber auch auf. Die Anzahl der Burgen in der Nalser Umgebung spricht zumindest dafür. In Nals befindet sich die <strong>Schwanburg</strong> die die  heutige Form vom Adelsgeschlecht Boymundt-Payersberg im 16. Jh. erhalten hat.<br/><br/>Ein typischer Frührenaissancebau ist hingegen die <strong>Stachelburg</strong>, in der heutzutage ein Restaurant betrieben wird. Etwas höher liegt die Ruine <strong>Pfeffersburg</strong> in Unterkasatsch. Sie wurde 1194 vom Bischof bewilligt und von den Herren von Tisens erbaut, ist jedoch seit dem 17. Jahrhundert nicht mehr bewohnt. Im nahen Umkreis der Ruine befinden sich jedoch schöne Familienwanderwege. Dazu kommt <strong>Schloss Payersberg</strong>, welches im 13. Jh. errichtet wurde und weit sichtbar auf einem Felsvorsprung thront. Die Burgen von Nals sind allemal einen Besuch und eine Wanderung wert.<br/><br/>Weitere Burgen und Anwesen, Kirchen und Klöster befinden sich in nächster Umgebung, darunter die Leonburg, Hocheppan oder die Ansitze im Burgendorf Prissian. ",
+      text_tmp:
+        "Nals ist für seine lange Geschichte und seine Siedlungsspuren aus vorromanischer Zeit bekannt. Im hohen Mittelalter blühte das Dorf aber auch auf. Die Anzahl der Burgen in der Nalser Umgebung spricht zumindest dafür.<br/><br/>In Nals befindet sich die <strong>Schwanburg</strong> die die  heutige Form vom Adelsgeschlecht Boymundt-Payersberg im 16. Jh. erhalten hat.<br/><br/>Ein typischer Frührenaissancebau ist hingegen die <strong>Stachelburg</strong>, in der heutzutage ein Restaurant betrieben wird.<br/><br/>Etwas höher liegt die Ruine <strong>Pfeffersburg</strong> in Unterkasatsch. Sie wurde 1194 vom Bischof bewilligt und von den Herren von Tisens erbaut, ist jedoch seit dem 17. Jahrhundert nicht mehr bewohnt. Im nahen Umkreis der Ruine befinden sich jedoch schöne Familienwanderwege.<br/><br/>Dazu kommt <strong>Schloss Payersberg</strong>, welches im 13. Jh. errichtet wurde und weit sichtbar auf einem Felsvorsprung thront.<br/><br/>Die Burgen von Nals sind allemal einen Besuch und eine Wanderung wert. Weitere Burgen und Anwesen, Kirchen und Klöster befinden sich in nächster Umgebung, darunter die Leonburg, Hocheppan oder die Ansitze im Burgendorf Prissian. ",
+      hero: {
+        path: "/slider/tinified/haus.png",
+        width: 1200,
+        height: 800,
+        imageAlt: "Foto des Hofes richtung Meran",
+      },
+    },
+    "naturbad-gargazon": {
+      title: "Naturbad Gargazon",
+      excerpt: "Eintauchen und genießen im Naturbad Gargazon",
+      text: "",
+      hero: {
+        path: "/slider/tinified/knotten.png",
+        width: 5456,
+        height: 3637,
+        imageAlt: "Foto des Hofes",
+      },
+    },
+    "schloss-tirol": {
+      title: "Schloss Tirol",
+      excerpt: "Das historisch bedeutendste Schloss Südtirols",
+      text: "Schloss Tirol ist das historisch bedeutendste Schloss Südtirols. Seine Entstehungsgeschichte reicht bis ins 11. Jahrhundert zurück und war die einstige Stammburg der Grafen Tirols. Heute beherbergt Schloss Tirol das Südtiroler Landesmuseum für Kultur- und Landesgeschichte sowie das Pflegezentrum für Vogelfauna mit seiner beeindruckenden Greifvogel-Flugschau.",
+      hero: {
+        path: "/slider/tinified/knotten.png",
+        width: 5456,
+        height: 3637,
+        imageAlt: "Foto des Hofes",
+      },
+    },
+    knottenkino: {
+      title: "Knottenkino",
+      excerpt: "Wanderung zum Knottenkino in Vöran",
+      text: "Der <strong>Aussichtspunkt</strong> mit 30 Kinosesseln aus Stahl und Kastanienholz, lädt Wanderer ein, Platz zu nehmen und die einmalige Aussicht wie einen Film im Kino zu genießen.<br/><br/><strong>„Knottn“</strong> bedeutet im Südtiroler Dialekt „Fels“.",
+      hero: {
+        path: "/slider/tinified/knotten.png",
+        width: 1200,
+        height: 800,
+        imageAlt: "Foto des Hofes",
+      },
+    },
+  },
+  request: {
+    title: "Einfach genießen",
+    intro:
+      "Wir freuen uns auf dich und erstellen so schnell wie möglich ein individuelles Angebot für dich.",
+    yourEnquiry: "Ihre Anfrage",
+    adults: "Erwachsene",
+    children: "Erwachsene",
+    contactData: "Kontaktdaten",
+    firstName: "Vorname",
+    lastName: "Nachname",
+    email: "E-Mail",
+    tel: "Telefon",
+    street: "Straße",
+    zipCode: "Plz",
+    location: "Ort",
+    country: "Land",
+    yourMessage: "Ihre Nachricht",
+    privacy: "Datenschutz",
+    sendRequest: "Anfrage senden",
+  },
+  footer: {
+    imprint: "Impressum",
+    privacy: "Datenschutz",
+    cookies: "Cookies",
+    address: "I - 39010 Nals - Manteleweg 2",
+    route: "Anfahrt",
+    vatId: "MwSt. Nr. IT 01754860219",
+  },
+};
+
+export default { de, it, en };
